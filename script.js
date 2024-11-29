354b644357343841102f75fddc3fd007e5de3c17
Munnacs2024 <111278268+Munnacs2024@users.noreply.github.com>
2024-11-29 13:04:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = '601248846206-gnvao70v6fvrj9rsqcnegtghdoq7a583.apps.googleusercontent.com'; // Replace with your Client ID
-const API_KEY = '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';     // Replace with your API Key
+const CLIENT_ID = 'abc'; // Replace with your Client ID
+const API_KEY = 'xxxx';     // Replace with your API Key
 const SCOPES = 'https://www.googleapis.com/auth/drive.file';
 const REDIRECT_URI = 'https://munnacs2024.github.io/photo-upload-website/'; // Your GitHub Pages URL
 
